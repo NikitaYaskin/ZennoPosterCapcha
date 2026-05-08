419aadb5a78ccf13acc816ff8cb01368b2a89a2d
Nikita <nikuuu66@gmail.com>
2026-05-08 17:01:03 +0300

    Update capcha2.py

--- capcha2.py
+++ capcha2.py
@@ -1,105 +1,109 @@
-import pyautogui, time, datetime, logging, os, pdb
+import pyautogui
+import time
+import datetime
+import logging
+import os
 
-logging.basicConfig(filename='capcha.log',level=logging.DEBUG)
+# 1. OPTIMIZATION: Let the logging module handle timestamps automatically.
+logging.basicConfig(
+    filename='captcha.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d-%m-%y %H:%M:%S'
+)
 
-def currentDateTime():
-	'''Informathon about date time'''
-	unix = int(time.time())
-	now = str(datetime.datetime.fromtimestamp(
-		unix).strftime('%d-%m-%y %H:%M:%S'))
-	return now
+def load_images(img_dir="img"):
+    """Loads images and automatically separates digit files from the title."""
+    data = {}
+    digits = []
+    
+    # Ensure directories exist so pyautogui.screenshot doesn't crash
+    os.makedirs(img_dir, exist_ok=True)
+    os.makedirs('failed_captcha', exist_ok=True)
+    os.makedirs('success', exist_ok=True)
 
-def convertIfInteger(string):
-        try:
-                return int(string)
-        except ValueError:
-                return string
+    for filename in os.listdir(img_dir):
+        name, _ = os.path.splitext(filename) # Splits '1.png' into '1' and '.png'
+        path = os.path.join(img_dir, filename)
+        
+        if name.isdigit():
+            key = int(name)
+            data[key] = path
+            digits.append(key)
+        else:
+            data[name] = path # Handles 'title.png' automatically
+            
+    logging.info(f'Loaded images from folder: {list(data.values())}')
+    return data, sorted(digits)
 
-def leftOnlyDigits(digits):
-        try:
-                return int(digits)
-        except ValueError:
-                pass
+# Initialize data and variables
+data, digits_on_screen = load_images()
+num_captchas = 0
 
-def findingImage():
-        data = {}
-        images = os.listdir("img/")
-        for image in images:
-            res = image.split('.')
-            key = convertIfInteger(res[0])
-            data[key] = str('img/') + image
-        logging.info('All files in the img folder\n{}'.format(data.values()))
-        return data
+window_location = (670, 380, 330, 270) 
+digit_location = (691, 457, 300, 146) 
+enter_button = (828, 621) 
 
-def formatAllKeys(data):
-        result = list()
-        for item in data:
-                if leftOnlyDigits(item) != None:
-                        result.append(leftOnlyDigits(item))
-        return result
+logging.info('Downloading the program / Starting script')
 
-numCapcha = 0
-numFaildCapchas = 0
-data = findingImage()
-digitsOnScreen = formatAllKeys(data)
-
-logging.info('Downloading the program\n {}'.format(currentDateTime()))
-
-windowLocation = (670, 380, 330, 270) 
-digitLocation = (691, 457, 300, 146) # Placing numbers in the window.
-enter = (828, 621) # Location of the enter key
-
-def faildCapcha(info):
-        faildCapchaName = 'faildCapcha/'+ str(info) + '.png'
-        pyautogui.screenshot(faildCapchaName, region=windowLocation)
-        logging.warning('Captcha not entered {}'.format(currentDateTime()))
+def save_failed_captcha(info_keys):
+    """Saves a screenshot with a unique timestamp to prevent overwriting."""
+    timestamp = datetime.datetime.now().strftime("%H%M%S")
+    filename = f'failed_captcha/failed_{info_keys}_{timestamp}.png'
+    pyautogui.screenshot(filename, region=window_location)
+    logging.warning(f'Captcha failed or not entered. Digits found: {info_keys}')
 
 while True:
-        dateInfo = []
+    # 2. OPTIMIZATION: Use .get() to prevent KeyErrors if 'title' is missing
+    if pyautogui.locateOnScreen(data.get('title'), region=window_location, grayscale=True):
         info = {}
-        if pyautogui.locateOnScreen(data['title'], region=windowLocation, grayscale=True):
-                for digit in digitsOnScreen:
-                        if pyautogui.locateOnScreen(data[digit], region=digitLocation, grayscale=True):
-                                info[digit] = pyautogui.locateCenterOnScreen(data[digit], region=digitLocation, grayscale=True)
-                                logging.info('Number found {0}'.format(info[digit]))
+        
+        for digit in digits_on_screen:
+            # 3. OPTIMIZATION: Only search the screen once per digit
+            center = pyautogui.locateCenterOnScreen(data[digit], region=digit_location, grayscale=True)
+            if center:
+                info[digit] = center
+                logging.info(f'Number {digit} found at {center}')
 
-                sortedInfo = sorted(info)
+        # 4. OPTIMIZATION: Cleaned up conditional logic
+        # Assuming exactly 4 digits are required to solve the CAPTCHA based on your original logic
+        if len(info) != 4:
+            pyautogui.alert(f'Incorrect number of digits found: {list(info.keys())}')
+            save_failed_captcha(list(info.keys()))
+            continue 
 
-                if len(sortedInfo) >= 0 and len(sortedInfo) <= 3 or len(sortedInfo) > 4:
-                        pyautogui.alert('Incorrect number of digits \n {}'.format(sortedInfo))
-                        logging.warning('Incorrect number of digits')
-                        logging.warning('{}'.format(sortedInfo))
-                        faildCapcha(sortedInfo)
-                        continue
-                elif len(sortedInfo) != []:
-                        pyautogui.moveTo(info[sortedInfo[0]][0], info[sortedInfo[0]][1])
+        # Extract sorted keys to draw the lines in numerical order
+        sorted_keys = sorted(info.keys())
+        first_point = info[sorted_keys[0]]
 
-                pyautogui.mouseDown()
+        # Move to the first digit and hold the mouse down
+        pyautogui.moveTo(first_point.x, first_point.y)
+        pyautogui.mouseDown()
+        logging.info('Left mouse button pressed')
 
-                logging.info('Left mouse button pressed {0}'.format(currentDateTime()))
+        # Drag through all points
+        for key in sorted_keys:
+            pyautogui.moveTo(x=info[key].x, y=info[key].y)
+            logging.info(f'Cursor moved to point {key}')
 
-                for key in sorted(info):
-                        pyautogui.moveTo(x=info[key][0], y=info[key][1])
-                        logging.info('The cursor moves to the point {0}'.format(key))
+        pyautogui.mouseUp()
+        logging.info('Left mouse button released')
 
-                pyautogui.mouseUp()
-                
-                logging.info('Left mouse button released')
-                
-                capcha = 'success/' + str(datetime.date.today()) + ' ' + str(info.keys()) + '.png'
-                pyautogui.screenshot(capcha, region=windowLocation)
-                
-                logging.info('Screenshot taken')
-                
-                pyautogui.click(enter)
-                logging.info('Pressed Enter')
-                time.sleep(1)
+        # Save success screenshot
+        timestamp = datetime.datetime.now().strftime("%H%M%S")
+        success_file = f'success/success_{timestamp}_{list(info.keys())}.png'
+        pyautogui.screenshot(success_file, region=window_location)
+        logging.info('Success screenshot taken')
 
-                if pyautogui.locateOnScreen(data['title'], region=windowLocation, grayscale=True):
-                        faildCapcha(sortedInfo)
-                        continue
-                else:
-                        numCapcha += 1
-                        logging.info('Pressed Enter. \nEntered {0} captchas. {1}'.format(numCapcha, currentDateTime()))
-                        time.sleep(30)
-                        continue
+        pyautogui.click(enter_button)
+        logging.info('Pressed Enter')
+        time.sleep(1)
+
+        # Verify if the CAPTCHA was actually solved
+        if pyautogui.locateOnScreen(data.get('title'), region=window_location, grayscale=True):
+            save_failed_captcha(list(info.keys()))
+            continue
+        else:
+            num_captchas += 1
+            logging.info(f'Entered {num_captchas} captchas successfully.')
+            time.sleep(30)
